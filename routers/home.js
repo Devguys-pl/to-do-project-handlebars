f7e6a9bfd2b4a95d389bbc13b21e20e44a7f1766
Xnsky <arturnawrocki121@gmail.com>
2022-04-14 21:39:17 +0200

    add another one flash messages, fix bugs in style.css

--- routers/home.js
+++ routers/home.js
@@ -1,53 +1,43 @@
-require('dotenv').config();
-const {
-  Router
-} = require('express');
-const userMiddleware = require('../middleware/user.middleware');
-const {
-  UserRecord
-} = require("../records/user.record");
-const {
-  TodoRecord
-} = require("../records/todo.record");
-const {
-  URLSearchParams
-} = require('url');
+require("dotenv").config();
+const { Router } = require("express");
+const userMiddleware = require("../middleware/user.middleware");
+const { UserRecord } = require("../records/user.record");
+const { TodoRecord } = require("../records/todo.record");
+const { URLSearchParams } = require("url");
 const homeRouter = Router();
 
-homeRouter.get('/', async (req, res, next) => {
+homeRouter.get("/", async (req, res, next) => {
   if (!req.session.user) {
-    req.flash('userNotLogged', 'You are not logged. If you want to see own task you have to log in. Now you can only create tasks that will be saved temporarily in local storage for up to 24 hours and only available from one device. Otherwise, create an account and log in. ');
-    return res.render('home', {
+    req.flash(
+      "userNotLogged",
+      "You are not logged. If you want to see own task you have to log in. Now you can only create tasks that will be saved temporarily in local storage for up to 24 hours and only available from one device. Otherwise, create an account and log in. "
+    );
+    return res.render("home", {
       isLogged: false,
       message: {
-        userNotLogged: req.flash('userNotLogged'),
-      }
-    })
+        userNotLogged: req.flash("userNotLogged"),
+      },
+    });
   }
   const todosList = await TodoRecord.listAll(req.session.user.id);
   const isLogged = req.session.user.isLogged;
-  res.render('home', {
+  res.render("home", {
     isLogged,
     todosList,
     message: {
-      emptyField: req.flash('emptyField'),
-      successLogin: req.flash('successLogin'),
-      somethingWrong: req.flash('somethingWrong'),
-      userNotExist: req.flash('userNotExist'),
-      userExist: req.flash('userExist'),
-      successRegister: req.flash('successRegister'),
-      userLogout: req.flash('userLogout'),
-      successfulTaskRemoved: req.flash('successfulTaskRemoved'),
-      unSuccessfulTaskRemoved: req.flash('unSuccessfulTaskRemoved'),
-      successfulChangeTaskStatus: req.flash('successfulChangeTaskStatus'),
-
+      emptyField: req.flash("emptyField"),
+      successLogin: req.flash("successLogin"),
+      somethingWrong: req.flash("somethingWrong"),
+      userNotExist: req.flash("userNotExist"),
+      userExist: req.flash("userExist"),
+      successRegister: req.flash("successRegister"),
+      userLogout: req.flash("userLogout"),
+      successfulTaskRemoved: req.flash("successfulTaskRemoved"),
+      unSuccessfulTaskRemoved: req.flash("unSuccessfulTaskRemoved"),
+      successfulChangeTaskStatus: req.flash("successfulChangeTaskStatus"),
     },
   });
 });
-
-
-
-
 
 module.exports = {
   homeRouter,
